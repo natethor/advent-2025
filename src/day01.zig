5fcb29d6a105f60f2cc1d3bdebeaceca7add39ea
Nate <natethor@users.noreply.github.com>
2025-12-08 22:16:32 -0500

    adding day1 part2

--- src/day01.zig
+++ src/day01.zig
@@ -1,5 +1,10 @@
 const std = @import("std");
 
+const Result = struct {
+    part1: i64,
+    part2: i64,
+};
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
@@ -12,39 +17,57 @@ pub fn main() !void {
     const input = try file.readToEndAlloc(allocator, 1024 * 1024);
     defer allocator.free(input);
 
-    // Part 1
-    const part1_result = try solvePart1(allocator, input);
-    std.debug.print("Part 1: {d}\n", .{part1_result});
+    const results = try solveParts(input);
+
+    std.debug.print("Part 1: {d}\n", .{results.part1});
+    std.debug.print("Part 2: {d}\n", .{results.part2});
 }
 
-fn solvePart1(allocator: std.mem.Allocator, input: []const u8) !i64 {
-    _ = allocator;
-
+fn solveParts(input: []const u8) !Result {
     var zero_count: i64 = 0;
+    var loop_count: i64 = 0;
     var curr_position: i16 = 50;
-    var curr_change: i16 = 0;
+    var curr_increment: i16 = 0;
+    var loops: i64 = 0;
 
     var lines = std.mem.splitScalar(u8, input, '\n');
     while (lines.next()) |line| {
         if (line.len == 0) continue; // skip empty lines
 
-        // std.debug.print("curr_position: {d}\n", .{curr_position});
-        if (line[0] == 'L') {
-            curr_change = try std.fmt.parseInt(i16, line[1..], 10);
-            curr_position = @mod(curr_position - curr_change + 100, 100);
+        curr_increment = try std.fmt.parseInt(i16, line[1..], 10);
+        const old_position = curr_position;
 
+        if (line[0] == 'L') {
+            const delta = curr_position - curr_increment;
+            curr_position = @mod(delta, 100);
+            loops = @divFloor(delta, 100);
+
+            // when starting at 0, we overcount by 1
+            if (old_position == 0 and loops != 0) {
+                loops += 1;
+            }
+
+            // when delta is exactly divisible by 100, we land on 0 and undercount by 1
+            // thanks reddit for helping with this edge case
+            if (delta < 0 and @mod(delta, 100) == 0) {
+                loops -= 1; // -1 becomes -2, so abs is larger
+            }
         } else if (line[0] == 'R') {
-            curr_change = try std.fmt.parseInt(i16, line[1..], 10);
-            curr_position = @mod(curr_position + curr_change + 100, 100);
+            const delta = curr_position + curr_increment;
+            curr_position = @mod(delta, 100);
+            loops = @divFloor(delta, 100);
         }
 
         if (curr_position == 0) {
             zero_count += 1;
-            // std.debug.print("zero_count: {d}\n", .{zero_count});
         }
-        // reset curr_change value during each loop
-        curr_change = 0;
+
+        if (curr_position == 0 and loops == 0) {
+            loop_count += 1;
+        } else {
+            loop_count = loop_count + @as(i64, @intCast(@abs(loops)));
+        }
     }
 
-    return zero_count;
+    return Result{ .part1 = zero_count, .part2 = loop_count };
 }
